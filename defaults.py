0cba7dba285ad2a4bc1d7750878dfcdc36e572ee
enigma-7 <aabohra17@gmail.com>
2020-05-11 16:12:51 +0200

    Added documentation

--- defaults.py
+++ defaults.py
@@ -2,5 +2,5 @@ directory = {"Resource"     : '../resources/',
              "Response"     : 'responses/',
              "Bibliography" : 'bibliography/'}
 
-separator = "\t"
-title_len = 48
+separator = "\t"                    #Separator for .txt file
+title_len = 48                      #Length of title in .txt
